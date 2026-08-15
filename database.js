80f7065a93118b479090f4fbe522a5e2cba17138
Rohan <rohanabraham06@gmail.com>
2026-08-14 20:33:12 -0400

    Use DB_PATH env var for persistent database location

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const initSqlJs = require('sql.js');
 const fs = require('fs');
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, 'gymtracker.db');
+const DB_PATH = process.env.DB_PATH || path.join(__dirname, 'gymtracker.db');
 
 function createWrapper(sqlDb) {
   function save() {
